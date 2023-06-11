07925ffbdd888bafd970b01fbc842b291869fce6
Alvaro Gamez Chavez <gaositd@gmail.com>
2023-06-10 23:12:24 -0600

    proyecto 1 finalizado

--- proyecto1-reactjs/src/components/Testimonio.jsx
+++ proyecto1-reactjs/src/components/Testimonio.jsx
@@ -6,22 +6,23 @@ export function Testimonio(props) {
 	return (
 		testimonio.map((data, index) => (
 			<div key={index} className='contenedorTestimonio'>
-				{
-					<>
-						<img
-							className='imagenTestimonio'
-							src={'../images/testimonio-' + data.imagen + '.png'}
-							alt={data.nombre}
+			{
+				<>
+					<img
+						className='imagenTestimonio'
+						src={'./images/testimonio-' + data.imagen + '.png'}
+						alt={data.imagen}
 						/>
-						<div className="contenedorTextoTestimonio">
-							<p className='nombreTestinomonio'>{data.nombre} en {data.pais}</p>
-							<p className='cargoTestinomonio'>{data.cargo}</p>
-							<p className='textoTestinomonio'>
-								{data.testimonio}
-							</p>
-						</div>
-					</>
-				}
+					<div className="contenedorTextoTestimonio">
+						<p className='nombreTestinomonio'>
+							<strong>{data.nombre}</strong> en {data.pais}</p>
+						<p className='cargoTestinomonio'>{data.cargo} en <strong>{data.empresa} </strong></p>
+						<p className='textoTestinomonio'>
+							{data.testimonio}
+						</p>
+					</div>
+				</>
+			}
 			</div>
 		))
 	);
